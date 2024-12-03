95d12e4cbe487df82988f7be9c3884f822a32d92
Wéberson Oliveira <154702444+Webers0n@users.noreply.github.com>
2024-12-03 15:51:52 -0300

    Create main.py
    Calcular Media de Alunos.

---
+++ Calculadora de Media/main.py
@@ -0,0 +1,25 @@
+situacao = dict()
+dados = list()
+
+while True:
+    o = int(input('CALCULAR NOTAS? [ 1 - INICIAR | 0 - ENCERRAR ]: '))
+
+    if o == 1 :
+        situacao['nome'] = str(input("Nome: "))
+        situacao['nota'] = int(input("Nota: "))
+        dados.append(situacao.copy())
+        
+    elif o == 0:
+        print("\nINFORMAÇÕES")
+        for i in dados:
+            # Determina a situação do aluno
+            if i['nota'] >= 60:
+                i['info'] = "Aprovado"
+            else:
+                i['info'] = "Reprovado"
+            
+            # Imprime as informações do aluno
+            print(f"Aluno(a)= {i['nome']} | Nota= {i['nota']} | Status= {i['info']}")
+        
+        break
+        
